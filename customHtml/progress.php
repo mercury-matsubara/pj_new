798ed9acc8502dd0c648b1a7a6c6307b574fe58a
yuya_ishino <ishino-yuya@mercury-soft.co.jp>
2020-08-29 02:01:00 +0900

    ポップアップ作成一時保存

--- customHtml/progress.php
+++ customHtml/progress.php
@@ -1,7 +1,38 @@
 <?php
+/**
+ * PJ進捗画面
+ * 
+ */
 class Progress extends InsertPage
 {
     public $errorCode = "";
+    
+    /**
+     * 関数名: makeStylePart
+     *   CSS定義文字列(HTML)を作成する関数
+     * (基本的にはCSSファイルへのリンクを作成)
+     * 
+     * @retrun HTML文字列
+     */
+    function makeStylePart() {
+        $html = parent::makeStylePart();
+        $html .= '<link rel="stylesheet" type="text/css" href="./customCSS/progresspopup.css">';
+        return $html;
+    }
+
+    /**
+     * 関数名: makeScriptPart
+     *   JavaScript文字列(HTML)を作成する関数
+     *   HEADタグ内に入る
+     *   使用するスクリプトへのリンクや、スクリプトの直接記述文字列を作成
+     * 
+     * @retrun HTML文字列
+    */
+    function makeScriptPart() {
+        $html = parent::makeScriptPart();
+        $html .= '<script src="./customJS/popup.js"></script>';
+        return $html;
+    }
     /**
      * 関数名: makeBoxContentMain
      *   メインの機能提供部分の上部に表示されるHTML文字列を作成する
@@ -66,7 +97,8 @@ class Progress extends InsertPage
         $html .= $header;
         $html .= '</div>';
         $html .= $tab;
-
+        // ポップアップ作成
+        $html .= $this->createPopUp();
         return $html;
     }
     /**
@@ -99,4 +131,5 @@ class Progress extends InsertPage
         $html .= '</script>';
         return $html;
     }
+   
 }
--- f_DB.php
+++ f_DB.php
@@ -1667,7 +1667,7 @@ function getCode($filename,$post="")
     {
         $code = "5CODE";
     }
-    else if($filename === "PROGRESSINFO_1" || $filename === "PROGRESSINFO_3")  //PJ進捗
+    else if($filename === "PROGRESSINFO_1" || $filename === "PROGRESSINFO_3" || $filename === "PROGRESSPOPUP_1")  //PJ進捗
     {
         $code = "7CODE";
     }
